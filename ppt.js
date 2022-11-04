670bfe9b5f165544cd8d1c22ec38403e14355574
javier <cesarquinonezmacuri@gmail.com>
2022-11-04 11:04:36 -0400

    un juego a piedra papel tijeras

---
+++ ppt.js
@@ -0,0 +1,52 @@
+function aleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function eleccion(jugada) {
+  let resultado = "";
+  if (jugada == 1) {
+    resultado = "piedra🌚";
+  } else if (jugada == 2) {
+    resultado = "pape📃";
+  } else if (jugada == 3) {
+    resultado = "tijera✂";
+  } else {
+    resultado = "ERROR...!😜";
+  }
+  return resultado;
+}
+
+// 1 es piedra , 2 es papel , 3 es tijera
+let jugador = 0;
+let pc = 0;
+let triunfos = 0;
+let perdidas = 0;
+
+while (triunfos < 3 && perdidas < 3) {
+  pc = aleatorio(1, 3);
+  jugador = prompt("Elije: 1 para piedra, 2 para papel, 3 para tijera");
+  // alert("Elegiste " + jugador)
+
+  alert("Pc elige " + eleccion(pc));
+  alert("Tu eliges " + eleccion(jugador));
+
+  //combate
+
+  if (pc == jugador) {
+    alert("EMPATE...!😒");
+  } else if (jugador == 1 && pc == 3) {
+    alert("GANASTE");
+    triunfos = triunfos + 1;
+  } else if (jugador == 3 && pc == 2) {
+    alert("GANASTE");
+    triunfos = triunfos + 1;
+  } else if (jugador == 2 && pc == 1) {
+    alert("Ganaste");
+    triunfos = triunfos + 1;
+  } else {
+    alert("PERDISTE");
+    perdidas = perdidas + 1;
+  }
+}
+
+alert("Ganaste " + triunfos + " veces. Perdiste " + perdidas + "  veces. ");
